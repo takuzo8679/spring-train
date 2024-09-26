61216704e2562925dce53b8c728f8b23307763db
takuzo <takuya.koiso.s@gmail.com>
2024-09-26 16:18:26 +0900

    chapter 10, 0801, SpringBoot and its configulation

--- 0801-training-boot/src/main/java/com/example/training/TrainingApplication.java
+++ 0801-training-boot/src/main/java/com/example/training/TrainingApplication.java
@@ -3,17 +3,10 @@ package com.example.training;
 import javax.sql.DataSource;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.jdbc.support.JdbcTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.example.training.entity.Reservation;
 import com.example.training.input.ReservationInput;
@@ -22,30 +15,11 @@ import com.example.training.service.ReservationService;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 
-@Configuration
-@ComponentScan
-@EnableTransactionManagement
+@SpringBootApplication
 public class TrainingApplication {
-    @Bean
-    public DataSource dataSource() {
-        EmbeddedDatabase dataSource = new EmbeddedDatabaseBuilder()
-                .addScripts("schema.sql", "data.sql")
-                .setType(EmbeddedDatabaseType.H2).build();
-        return dataSource;
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
-
-    @Bean
-    public JdbcTransactionManager transactionManager(DataSource dataSource) {
-        return new JdbcTransactionManager(dataSource);
-    }
-
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(TrainingApplication.class);
+//        ApplicationContext context = new AnnotationConfigApplicationContext(TrainingApplication.class);
+        ApplicationContext context = SpringApplication.run(TrainingApplication.class, args);
         // トランザクション制御のログを出力してくれるように設定
         ((Logger) LoggerFactory.getLogger(JdbcTransactionManager.class)).setLevel(Level.DEBUG);
         ReservationService reservationService = context.getBean(ReservationService.class);
